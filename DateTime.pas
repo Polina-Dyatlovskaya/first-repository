7d1e5248d563b3a8bcbf833e492c319eb68c0b34
Polina Dyatlovskaya <dyatlowskaya.polina@yandex.ru>
2024-10-04 22:26:37 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -1,5 +1,8 @@
 ﻿begin
   var year := ReadInteger('Введите целое положительное число - год:');
   Assert(year > 0);
-  Print($'Является ли год високосным: {((year.Divs(4)) and (year mod 100 <> 0)) or (year.Divs(400))}');
+  Print($'Является ли год високосным: {((year.Divs(4)) and (year mod 100 <> 0)) or (year.Divs(400))}'); // функционал, вычисляющий високосность года
+  
+  var second := 60;
+  Print($'{newline}{newline}В одной минуте {second} секунд.');  // функционал, выводящий количество секунд в минуте
 end.
