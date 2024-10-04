5bf478905e439381d170abb51ad3d477bbf5db06
Polina Dyatlovskaya <dyatlowskaya.polina@yandex.ru>
2024-10-04 22:15:31 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var year := ReadInteger('Введите целое положительное число - год:');
+  Assert(year > 0);
+  Print($'Является ли год високосным: {((year.Divs(4)) and (year mod 100 <> 0)) or (year.Divs(400))}');
+end.
